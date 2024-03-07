51c193e9708b575d0c4a4401692fd370be67790a
Omar-Magdy0 <omar.magdy.om.om@gmail.com>
2024-03-07 04:52:21 +0200

    update

--- src/ControllerCore.cpp
+++ src/ControllerCore.cpp
@@ -1,19 +1,29 @@
 #include "ControllerCore.h"
 #include <string.h>
 #include <util/delay.h>
-
+#include <stdint.h>
 
 
 extern uint16_t globalTimeMillis;
+
+
+/*
+DUMMY struct + OBJECT FOR C/C++ test USING AVR-GCC COMPILER
+*/
+
+
+
 int main(){
+ 
   setupFunction();
   Serial0.begin(9600);
   clk0CountMillis(START);
+  _delay_ms(50);
+
+
   
   while (1)
   { _delay_ms(500);
-    Serial0.print("FUCK  YOU");
-    Serial0.print('\n');
    }
   return 0;
 }
