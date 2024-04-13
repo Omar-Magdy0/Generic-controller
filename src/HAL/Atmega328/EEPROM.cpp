3ec4da279a020b982a9337da6d48d5df812e351a
Omar Magdy <omar.magdy.Om.Om@gmail.com>
2024-04-13 13:42:25 +0200

    update

--- src/HAL/Atmega328/EEPROM.cpp
+++ src/HAL/Atmega328/EEPROM.cpp
@@ -1,1 +1,11 @@
 #include "EEPROM.h"
+ 
+ void EEPROM_READ(int pos,const char *data,uint8_t size){
+    while(size--){
+        
+    }
+ }
+
+ void EEPROM_WRITE(int pos, const char *data, uint8_t size){
+
+ }
--- src/HAL/Atmega328/EEPROM.h
+++ src/HAL/Atmega328/EEPROM.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #include "avr/eeprom.h"
+
+void EEPROM_READ(int pos,const char *data,uint8_t size);
+void EEPROM_WRITE(int pos, const char *data, uint8_t size);
